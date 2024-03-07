2224eb26834bae6030b8bd1256cd0205b5c2b8b2
Agustín Bravo Pérez <anbravoperez@gmail.com>
2024-03-07 02:07:42 -0300

    feat: GitHub Action para deployar en GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: { provider: "plausible" },
     locale: "es-ES",
     baseUrl: "agustinbravop.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian", "Excalidraw"],
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       cdnCaching: true,
